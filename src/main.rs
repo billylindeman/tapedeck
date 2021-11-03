008c49a3f587806d6fa760bd87d64876861a0f45
Billy Lindeman <billylindeman@gmail.com>
2021-11-03 12:00:58 -0700

    working pulse audio

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,43 @@
-use std::ffi::OsStr;
+use headless_chrome::{protocol::page::ScreenshotFormat, Browser, LaunchOptions};
 use std::collections::HashMap;
-use x11rb::protocol::xproto::{Atom, AtomEnum, ConnectionExt, GetWindowAttributesReply};
-use x11rb::connection::Connection;
 use std::error::Error;
+use std::ffi::OsStr;
 use std::result::Result;
-use headless_chrome::{Browser, LaunchOptions, protocol::page::ScreenshotFormat};
+use x11rb::connection::Connection;
+use x11rb::protocol::xproto::{Atom, AtomEnum, ConnectionExt, GetWindowAttributesReply};
 
-use subprocess::{Exec, CaptureData};
+use std::sync::mpsc::channel;
 
+use subprocess::{CaptureData, Exec};
 
 fn launch_chrome_browser(display: &str) -> Result<Browser, Box<dyn Error>> {
     let mut env = HashMap::new();
+    env.insert("PULSE_SERVER".to_owned(), "tcp:localhost:5546".to_owned());
+    env.insert("PULSE_SINK".to_owned(), "loopback".to_owned());
     env.insert("DISPLAY".to_owned(), display.to_owned());
 
     let mut args = Vec::new();
-    args.push(OsStr::new("--class=-record123"));
+    args.push(OsStr::new("--enable-audio-output"));
+    args.push(OsStr::new("--autoplay-policy=no-user-gesture-required"));
+    args.push(OsStr::new("--disable-infobars"));
+    args.push(OsStr::new("--kiosk"));
+    args.push(OsStr::new("--disable-automation"));
+    println!("ENV: {:?}", env);
 
     let options = LaunchOptions::default_builder()
         .headless(false)
+        .window_size(Some((1920, 1080)))
+        .sandbox(false)
         .process_envs(Some(env))
         .args(args)
         .build()?;
+
     let browser = Browser::new(options)?;
 
     let tab = browser.wait_for_initial_tab()?;
 
     // Navigate to wikipedia
-    tab.navigate_to("https://youtube.com")?;
+    tab.navigate_to("https://www.youtube.com/watch?v=XhRIqzUDqAM")?;
 
     // Wait for network/javascript/dom to make the search-box available
     // and click it.
@@ -35,9 +46,17 @@ fn launch_chrome_browser(display: &str) -> Result<Browser, Box<dyn Error>> {
     Ok(browser)
 }
 
+fn launch_pulse() -> Result<Exec, Box<dyn Error>> {
+    Ok(Exec::shell(format!(
+        "dbus-run-session pulseaudio -n -F config/pulse.pa -vvv --disable-shm --use-pid-file=false --realtime=false"
+    )))
+}
 
-fn launch_xvfb(display: &str) -> Result<Exec, Box<dyn Error>> {
-    Ok(Exec::shell(format!("Xvfb {}", display)))
+fn launch_xvfb(display: &str, size: (u32, u32)) -> Result<Exec, Box<dyn Error>> {
+    Ok(Exec::shell(format!(
+        "Xvfb {} -screen 0 {}x{}x24",
+        display, size.0, size.1
+    )))
 }
 
 fn list_window_classes(display: &str) -> Result<(), Box<dyn Error>> {
@@ -50,59 +69,77 @@ fn list_window_classes(display: &str) -> Result<(), Box<dyn Error>> {
     // Get the already existing top-level windows.
     let tree_reply = conn.query_tree(screen.root)?.reply()?;
 
-
     println!("got tree reply");
 
     // Iterate windows and find the chrome-browser
     for win_id in tree_reply.children {
         println!("got window: {}", win_id);
 
-        let reply = conn
-            .get_property(
-                false,
-                win_id,
-                AtomEnum::WM_NAME,
-                AtomEnum::STRING,
-                0,
-                std::u32::MAX,
-            )?;
+        let reply = conn.get_property(
+            false,
+            win_id,
+            AtomEnum::WM_NAME,
+            AtomEnum::STRING,
+            0,
+            std::u32::MAX,
+        )?;
         let title = reply.reply()?.value;
 
-        let reply = conn
-            .get_property(
-                false,
-                win_id,
-                AtomEnum::WM_CLASS,
-                AtomEnum::STRING,
-                0,
-                std::u32::MAX,
-            )?;
+        let reply = conn.get_property(
+            false,
+            win_id,
+            AtomEnum::WM_CLASS,
+            AtomEnum::STRING,
+            0,
+            std::u32::MAX,
+        )?;
         let class = reply.reply()?.value;
         let class = String::from_utf8(class)?;
         let split: Vec<_> = class.split('-').collect();
 
-        println!("got title: {} => class: {:?}", String::from_utf8(title)?, split);
+        println!(
+            "got title: {} => class: {:?}",
+            String::from_utf8(title)?,
+            split
+        );
     }
-
 
     Ok(())
 }
 
-fn main() -> Result<(), Box<dyn Error>>{
+fn main() -> Result<(), Box<dyn Error>> {
+    pretty_env_logger::init();
 
     let display: &str = ":1234";
 
-
+    let (wait_xvfb_tx, wait_xvfb_rx) = channel();
     std::thread::spawn(move || {
         println!("launching xvfb");
-        let x = launch_xvfb(display).unwrap();
-        x.join();
+        let x = launch_xvfb(display, (1920, 1080)).unwrap();
+
+        let mut pipe = x.popen().expect("error launching xvfb");
+        wait_xvfb_tx.send(true).unwrap();
+        pipe.wait().unwrap();
     });
+
+    let (wait_pulse_tx, wait_pulse_rx) = channel();
+    std::thread::spawn(move || {
+        println!("launching pulseaudio");
+        let x = launch_pulse().unwrap();
+
+        let mut pipe = x.popen().expect("error launching xvfb");
+        wait_pulse_tx.send(true).unwrap();
+        pipe.wait().unwrap();
+    });
+
+    wait_xvfb_rx.recv().unwrap();
+    println!("xvfb launched");
+    wait_pulse_rx.recv().unwrap();
+    println!("pulse launched");
 
     println!("launching chrome");
     let _b = launch_chrome_browser(display)?;
     list_window_classes(display)?;
-
 
     loop {}
 
